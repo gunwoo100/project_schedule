fef4e8351846c65c3007b43162ee225575f47523
gunwoo <gunwoo6121@gmail.com>
2025-03-13 12:41:48 +0900

    Update ToDoService.java

--- app/src/main/java/com/example/myapplication/Service/ToDoService.java
+++ app/src/main/java/com/example/myapplication/Service/ToDoService.java
@@ -7,19 +7,25 @@ import java.util.ArrayList;
 import retrofit2.Call;
 
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ToDoService {
 
-    @POST("/rest/test/create")
-    Call<Integer> createToDoData(@Body ToDoClass todo);
+    @POST("/rest/scheduleApp/todo/create")
+    Call<ArrayList<ToDoClass>> createToDoData(@Body ToDoClass todo);
 
-    @GET("/rest/test/getToDo")
+    @GET("/rest/scheduleApp/todo/get")
     Call<ArrayList<ToDoClass>> getToDoData();
 
-    @PATCH("/rest/test/edit")
-    Call<Integer> editToDoData(@Body ToDoClass todo);
+    @PATCH("/rest/scheduleApp/todo/edit/{id}")
+    Call<ArrayList<ToDoClass>> editToDoData(@Path("id") Long todo_id);
+
+    @DELETE("/rest/scheduleApp/todo/delete/{id}")
+    Call<ArrayList<ToDoClass>> deleteToDoData(@Path("id") Long id);
 
 }
+
