2d855d47964acfa66f18f38c4bd229a8fbea8f72
gunwoo <gunwoo6121@gmail.com>
2025-03-14 13:08:01 +0900

    Update ScheduleService.java

--- app/src/main/java/com/example/myapplication/Service/ScheduleService.java
+++ app/src/main/java/com/example/myapplication/Service/ScheduleService.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.Service;
 
 import com.example.myapplication.Class.ScheduleClass;
+import com.example.myapplication.RvAdapter_ViewHolder.MainRvAdapter;
 
 import java.util.ArrayList;
 
@@ -17,26 +18,35 @@ import retrofit2.http.Path;
 public interface ScheduleService {
 
     //GET
+    /**SearchScheduleActivity - {@link com.example.myapplication.Activity.SearchScheduleActivity}*/
     @GET("/rest/scheduleApp/schedule/getByYM/{year}/{month}")
     Call<ArrayList<ScheduleClass>> getDataListByYM(@Path("year")int year, @Path("month")int month);
-    @GET("/rest/scheduleApp/schedule/getByYMD/{year}/{month}/{day}")
-    Call<ArrayList<ScheduleClass>> getDataListByYMD(@Path("year")int year, @Path("month")int month, @Path("day")int day);
+
+    /**SearchScheduleActivity - {@link com.example.myapplication.Activity.SearchScheduleActivity}*/
     @GET("/rest/scheduleApp/schedule/getByYMC/{year}/{month}/{category}")
     Call<ArrayList<ScheduleClass>> getDataListByYMC(@Path("year")int year, @Path("month")int month, @Path("category")String category);
 
+    /**MainActivity - <b>displayDate()</b>*/
+    @GET("/rest/scheduleApp/schedule/getByYMD/{year}/{month}/{day}")
+    Call<ArrayList<ScheduleClass>> getDataListByYMD(@Path("year")int year, @Path("month")int month, @Path("day")int day);  //MainActivity - displayDate()
+
 
     //CREATE
-    @POST("/rest/scheduleApp/schedule/post")
+    /**CreateActivity - {@link com.example.myapplication.Activity.CreateActivity}*/
+    @POST("/rest/scheduleApp/schedule/create")
     Call<ScheduleClass> createSchedule(@Body ScheduleClass schedule);
 
 
     //EDIT
+    /**EditActivity - {@link com.example.myapplication.Activity.EditActivity}*/
     @PATCH("/rest/scheduleApp/schedule/edit/{id}")
-    Call<Void> editSchedule(@Path("id")Long id,@Body ScheduleClass schedule);
+    Call<Integer> editSchedule(@Path("id")Long id,@Body ScheduleClass schedule);
 
 
     //DELETE
+    /**SearchViewAdapter - {@link com.example.myapplication.RvAdapter_ViewHolder.SearchViewAdapter}<br>
+     * MyRvAdapter - {@link com.example.myapplication.RvAdapter_ViewHolder.MainRvAdapter}*/
     @DELETE("/rest/scheduleApp/schedule/delete/{id}")
-    Call<ArrayList<ScheduleClass>> deleteSchedule(@Path("id")Long id);
+    Call<Integer> deleteSchedule(@Path("id")Long id);
 
 }
